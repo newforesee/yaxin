6b0f2b45f721c23caebbfde7fcd550c99d482a3d
newforesee <newforesee@outlook.com>
2018-10-30 16:10:29 +0800

    create

---
+++ src/main/java/com/yaxin/bigdata/Test.java
@@ -0,0 +1,10 @@
+package com.yaxin.bigdata;
+
+/**
+ * creat by newforesee 2018/10/30
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
